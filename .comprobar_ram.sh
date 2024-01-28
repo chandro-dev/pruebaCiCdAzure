988975eeb14156853a947c190c4187092103f0ee
Luis ALejandro Carretero Ballesteros <alejo_carretero@hotmail.com>
2024-01-27 19:34:49 -0500

    la rama error: pathspec 'hola' did not match any file(s) known to git fue creada

---
+++ .comprobar_ram.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+
+# Verificar si la rama existe
+hola=$(git checkout $1 2>&1)
+
+# Verificar si el mensaje de error contiene la palabra "error"
+if echo "$hola" | grep -q 'error'; then	
+    
+	echo "La rama no existe."
+	git branch -b $hola
+	echo "rama creada"
+	git add . && git commit -m "la rama $hola fue creada"
+	git push origin $hola
+else
+    echo "La rama existe."
+fi
